4adfa62fc86edc8c73972e8822420a1357f7a3ae
KIU Shueng Chuan <nixchuan@gmail.com>
2025-03-19 07:03:02 +0800

    points: let user choose whether to use opengl

--- points-viewer/main.py
+++ points-viewer/main.py
@@ -1,20 +1,28 @@
-import sys
+import argparse
 
 import trimesh
 from PySide6 import QtWidgets
 
 from PointsRhiWidget import PointsRhiWidget
 
+def parse_api(api : str) -> QtWidgets.QRhiWidget.Api:
+    match api:
+        case 'opengl':
+            out = QtWidgets.QRhiWidget.Api.OpenGL
+        case 'vulkan':
+            out = QtWidgets.QRhiWidget.Api.Vulkan
+        case _:
+            raise ValueError(f"unrecognized api: {api}")
+    return out
+
 class Widget(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, api = None):
+        super().__init__()
         layout = QtWidgets.QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         self._rhi_widget = PointsRhiWidget(self)
-        Api = QtWidgets.QRhiWidget.Api
-        if self._rhi_widget.api() in [Api.Direct3D11, Api.Direct3D12]:
-            # DirectX does not support gl_PointSize
-            self._rhi_widget.setApi(Api.OpenGL)
+        if api is not None:
+            self._rhi_widget.setApi(parse_api(api))
         layout.addWidget(self._rhi_widget)
 
     def closeEvent(self, e):
@@ -24,10 +32,17 @@ class Widget(QtWidgets.QWidget):
         e.accept()
 
 if __name__ == '__main__':
-    mesh = trimesh.load_mesh(sys.argv[1])
+    # DirectX does not support gl_PointSize
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', nargs='?')
+    parser.add_argument('--api', choices=['opengl', 'vulkan'])
+    ARGS = parser.parse_args()
+
+    mesh = trimesh.load_mesh(ARGS.filename)
     app = QtWidgets.QApplication([])
-    win = Widget()
-    win.resize(640, 480)
+    win = Widget(ARGS.api)
     win._rhi_widget.setData(mesh)
+    win.resize(640, 480)
     win.show()
     app.exec()
